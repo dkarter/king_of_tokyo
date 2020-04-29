6909629df01fb0419587aa8d31dbac7d3471d6b0
Dorian Karter <dkarter@users.noreply.github.com>
2020-04-28 22:38:53 -0500

    Fix GIT_CLEAN_PATHS and add AUTO_VERSION
    - also run `mix phx.digest.clean` before digesting assets

--- lib/mix/tasks/build_js.ex
+++ lib/mix/tasks/build_js.ex
@@ -9,6 +9,7 @@ defmodule Mix.Tasks.BuildJs do
   def run(_args) do
     Mix.shell().cmd("mkdir -p priv/static")
     Mix.shell().cmd("yarn install --cwd assets && yarn --cwd assets deploy")
+    Mix.Task.run("phx.digest.clean")
     Mix.Task.run("phx.digest")
   end
 end
